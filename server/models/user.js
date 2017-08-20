e88ae775ce8ba4732b9e856466402d051f693044
William <william.gaiteiro@gmail.com>
2017-08-20 15:36:26 -0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -71,7 +71,6 @@ UserSchema.statics.findByToken = function(token) {
         // return new Promise((resolve, reject) => {
         //     reject();
         // });
-        
     }
 
     return User.findOne({
@@ -81,7 +80,25 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
-UserSchema.pre('save', function (next) {
+UserSchema.statics.findByCredentials = function(email, password) {
+    return User.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            })
+        });
+    });
+};
+
+UserSchema.pre('save', function(next) {
     var user = this;
 
     if (user.isModified('password')) {
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
-var {authenticate} = require('./middleware/authemticate');
+var { authenticate } = require('./middleware/authemticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -102,10 +102,6 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
-app.get('/users/me', authenticate, (req, res) => {
-    res.send(req.user);
-});
-
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password', 'tokens']);
     var user = new User(body);
@@ -119,6 +115,22 @@ app.post('/users', (req, res) => {
     });
 
     console.log(body);
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send("Not auth");
+    });
 });
 
 app.listen(port, () => {
